32ccf58eb211f8ab51778eb7a2fcf9f6f09274f5
David Gaspar <davidgaspar.dev@gmail.com>
2018-07-06 01:21:33 -0300

    Adding condiction of the input/ouput

--- convert.cpp
+++ convert.cpp
@@ -1,8 +1,50 @@
+/**
+ * @author davidgaspar.dev@gmail.com (David Gaspar)
+ */
 #include <iostream> // library for input and output
 
 int main() {
 
-  std::cout << "Hello World!" << std::endl;
+  // Classic Hello, World!
+  std::cout << "Hello, World!" << std::endl;
+
+  // Variable @type boolean
+  bool validatedData;
+  // Variable @type integer
+  int decimal;
+
+  do {
+
+    try {
+
+      // Console output
+      // @type char[]
+      std::cout << "Digite um número deciaml: ";
+
+      // Console input
+      // @type int (variable)
+      std::cin >> decimal;
+
+      // Condiction
+      if(!std::cin) {
+
+        validatedData = false;
+        throw "error";
+
+      }else {
+
+        validatedData = true;
+
+      }
+
+    }catch(int) {
+
+      std::cout << "e" << std::endl;
+      std::cin.clear(); // Clear error flags
+
+    }
+
+  }while(!validatedData);
 
   return 1;
 }
